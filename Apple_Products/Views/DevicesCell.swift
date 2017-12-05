63aa204bb3930b8bbf222cdf7c91013cb3c4707c
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-04 22:51:04 -0800

    DeviceCell: Created new class DeviceCell of CollectionViewCell.  Added IBOutlets and linked to DevicesVC storyboard.

---
+++ Apple_Products/Views/DevicesCell.swift
@@ -0,0 +1,24 @@
+//
+//  DevicesCellCell.swift
+//  Apple_Products
+//
+//  Created by Donald Belliveau on 2017-12-04.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class DevicesCell: UICollectionViewCell {
+    
+    /*
+     IBOutlets
+     */
+    
+    @IBOutlet weak var deviceImage: UIImageView!
+    @IBOutlet weak var deviceTitle: UILabel!
+    @IBOutlet weak var devicePrice: UILabel!
+    
+    
+}
+
+// DeviceCell: 
